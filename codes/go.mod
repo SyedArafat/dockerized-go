381cfafcb14c4e99f9123d28439439d23cec7b6d
Sayed Yeamin Arafat <syedyeaminarafat@gmail.com>
2023-10-30 15:47:44 +0600

    #rabbitmq connection go

--- codes/go.mod
+++ codes/go.mod
@@ -2,4 +2,8 @@ module export_nrt_report
 
 go 1.21.3
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
+	github.com/wagslane/go-rabbitmq v0.12.4 // indirect
+)
--- codes/main.go
+++ codes/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"export_nrt_report/packages/db"
 	"fmt"
+	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Hello Arafat")
 	initialize()
+	conn, err := amqp091.Dial(
+		"amqp://guest:guest@common-rabbitmq:5672",
+	)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
 }
 
 func initialize() {
